28c4f28dc443f6ef9debab292416574db2b0a14f
Allan Clark <allanc@chickenandporn.com>
2013-12-30 14:49:33 -0800

    formatting and whitespace

--- java/NoCapHeader.java
+++ java/NoCapHeader.java
@@ -24,18 +24,18 @@ public class NoCapHeader extends RowPrinter
     }
     public NoCapHeader (RowPrinter aParent)
     {
-	if (null == aParent)
-	    parent = new CsvRowPrinter();
-	else
+        if (null == aParent)
+            parent = new CsvRowPrinter();
+        else
             parent = aParent;
     }
 
     public void printHeaderRow (String[] data)
     {
-	Pattern p = Pattern.compile("(Total |)Capacity %");
+        Pattern p = Pattern.compile("(Total |)Capacity %");
 
-	for (int i = java.lang.reflect.Array.getLength(data); i > 0; i--)
-	    data[i-1] = p.matcher(data[i-1]).replaceAll("% Utilization");
+        for (int i = java.lang.reflect.Array.getLength(data); i > 0; i--)
+            data[i-1] = p.matcher(data[i-1]).replaceAll("% Utilization");
 
         parent.printHeaderRow(data);
     }
@@ -44,7 +44,7 @@ public class NoCapHeader extends RowPrinter
 
     public void printDataRow (String[] data)
     {
-            parent.printDataRow(data);
+        parent.printDataRow(data);
     }
 
     public void printFooterRow ()
